f262304b9f4164337d19ff078ac5cf29703f4d73
Haksung <159970028+Haksungkim0710@users.noreply.github.com>
2024-02-21 17:37:04 +0900

    Add files via upload

--- Lv.1_문자열_다루기_기본.py
+++ Lv.1_문자열_다루기_기본.py
@@ -1,5 +1,7 @@
 def solution(s):
+    #문자열 s의 길이가 4 or 6 인지 확인
     if len(s) == 4 or len(s) == 6:
+        #문자열 s가 '숫자'로만 이루어져있는지 확인
         if s.isdigit() == True:
             return True
         else:
--- Lv.1_부족한_금액_계산하기.py
+++ Lv.1_부족한_금액_계산하기.py
@@ -1,11 +1,13 @@
 def solution(price, money, count):
     sum = 0
 
+    #1부터 count까지 sum에 price와 횟수만큼 곱해준 값을 더한다
     for i in range(1, count+1):
         sum += price * i
-
+    #money에서 sum을 빼준다
     money -= sum
 
+    #money가 양수면 0을 출력, 음수면 abs로 절대값을 리턴
     if money > 0:
         return 0
     else:
